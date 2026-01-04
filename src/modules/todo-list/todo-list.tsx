d7b0ce307615d4493fa03d1aa6740aace666aecc
Tanusha Verksa <veraksa161@gmail.com>
2026-01-05 02:33:08 +0300

    add tailwind and pagination

--- src/modules/todo-list/todo-list.tsx
+++ src/modules/todo-list/todo-list.tsx
@@ -1,10 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { todoListApi } from "./api";
+import { useState } from "react";
 
 const TodoList = () => {
+  const [page, setPage] = useState(1);
+
   const { data, error, isPending } = useQuery({
     queryKey: ["tasks", "list"],
-    queryFn: todoListApi.getTodoList,
+    queryFn: (meta) => todoListApi.getTodoList({ page }, meta),
   });
 
   if (isPending) {
@@ -16,11 +19,27 @@ const TodoList = () => {
   }
 
   return (
-    <div>
-      TodoList
-      {data.map((todo) => (
-        <div key={todo.id}>{todo.text}</div>
-      ))}
+    <div className="p-5 mx-auto max-w-[1200px] mt-10">
+      <h1 className="text-3xl font-bold underline mb-5">TodoList</h1>
+      <div className="flex flex-col gap-4 mb-4  ">
+        {data.data.map((todo) => (
+          <div className="border border-slate-300 rounded p-3" key={todo.id}>
+            {todo.text}
+          </div>
+        ))}
+      </div>
+      <button
+        onClick={() => setPage((p) => Math.max(p - 1, 1))}
+        className="p-3 rounded border border-teal-500"
+      >
+        prev
+      </button>
+      <button
+        onClick={() => setPage((p) => Math.max(p + 1, data.pages))}
+        className="p-3 rounded border border-teal-500"
+      >
+        next
+      </button>
     </div>
   );
 };
